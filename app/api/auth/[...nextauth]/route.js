823fc554bbe5be22eb13ecccef16c828044e33a9
Thaddaeus <thaddaeusotieno@gmail.com>
2023-06-04 13:43:21 +0300

    ./

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 // import { signIn } from "next-auth/react";
 import { connectToDB} from "@utils/database";
+import User from  "@models/user";
 
 console.log()
 
@@ -14,16 +15,27 @@ const handler = NextAuth({
         })
     ],
 async session({session}){
-
+    const sessionUser = await User.findOne({
+        email: session.email.user,
+    })
+    session.user.id = sessionUser._id.toString();
 },
 async signIn({profile}){
     try {
         //serverless -> Lambda
         await connectToDB();
         //CHECK IF A USER ALREADY EXITS 
-
+        const userExist = await User.findOne({
+            email: profile.email
+        })
         //IF NOT, CREATE A NEW USER
-
+        if (!UserExist) {
+            await User.create({
+                email: profile.email,
+                username: profile.name.replace("","").toLowerCase(),
+                image: profile.picture
+            })
+        }
         return true;
     } catch (error) {
         console.log(error)
--- models/user.js
+++ models/user.js
@@ -16,6 +16,6 @@ const UserSchema = newSchema ({
     }
 })
 
-const User = model ("User", UserSchema);
+const User = models.User || model ("User", UserSchema);
 
 export default User;
