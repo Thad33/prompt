82bf163fc34ead1c5145945bd2fb12b3b8a01f78
Thaddaeus <thaddaeusotieno@gmail.com>
2023-06-07 03:51:20 +0300

    from.jsx

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,34 +14,38 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-async session({session}){
-    const sessionUser = await User.findOne({
-        email: session.email.user,
-    })
-    session.user.id = sessionUser._id.toString();
-},
-async signIn({profile}){
-    try {
-        //serverless -> Lambda
-        await connectToDB();
-        //CHECK IF A USER ALREADY EXITS 
-        const userExist = await User.findOne({
-            email: profile.email
-        })
-        //IF NOT, CREATE A NEW USER
-        if (!UserExist) {
-            await User.create({
-                email: profile.email,
-                username: profile.name.replace("","").toLowerCase(),
-                image: profile.picture
+
+    callbacks: {
+        async session({session}){
+            const sessionUser = await User.findOne({
+                email: session.email.user,
             })
+            session.user.id = sessionUser._id.toString();
+        },
+        async signIn({profile}){
+            try {
+                //serverless -> Lambda
+                await connectToDB();
+                //CHECK IF A USER ALREADY EXITS 
+                const userExist = await User.findOne({
+                    email: profile.email
+                })
+                //IF NOT, CREATE A NEW USER
+                if (!UserExist) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace("","").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch (error) {
+                console.log(error)
+                return false;
+            }
         }
-        return true;
-    } catch (error) {
-        console.log(error)
-        return false;
     }
-}
+
 }) 
 
 export { handler as GET, handler as POST };
